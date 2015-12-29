571462c49e568d2d8e8237c168182120d97f3e46
Frank Lee <lee.frankie@gmail.com>
2015-12-29 15:51:41 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    // $(".collapsible").collapsible({
+    //   accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    // });
+});
